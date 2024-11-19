36188215155e1b4fb1fb260ffa842f2f08d27fc1
PHAMQUOCAN03 <Phaman7823@gmail.com>
2024-11-19 23:42:29 +0700

    search 26/10

--- MvcDemo/obj/Debug/net8.0/MvcDemo.AssemblyInfo.cs
+++ MvcDemo/obj/Debug/net8.0/MvcDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963a866d6a88faacc4303e717905b55476a4ad2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6785d21d1445e8a2c0347caf4c3d9ab19c060f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
